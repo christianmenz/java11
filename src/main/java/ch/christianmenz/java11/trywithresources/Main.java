e0c037bcf06b6eed9eeb2c51d906781904986c64
Christian Menz <christianmenz@gmail.com>
2019-01-01 20:22:07 +0100

    try with resources and additional finally

---
+++ src/main/java/ch/christianmenz/java11/trywithresources/Main.java
@@ -0,0 +1,26 @@
+package ch.christianmenz.java11.trywithresources;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StringReader stringReader = new StringReader("test") {
+            @Override
+            public void close() {
+                System.out.println("Closing...");
+                super.close();
+            }
+        };
+        try (stringReader) { // 1rst
+            stringReader.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally { // 2nd
+            System.out.println("My finally block");
+            stringReader.close();
+            System.out.println(".. interesting");
+        }
+    }
+}
